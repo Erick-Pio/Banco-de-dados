13d26f699550f9618ad0aef3afc8846b38ed4b8e
Erick-Pio <erick.pio@sptech.school>
2022-04-11 14:33:50 -0300

    exercicio 07 bd ex 02

--- exercicio_07_ex_02.sql
+++ exercicio_07_ex_02.sql
@@ -7,7 +7,9 @@ idPessoa int primary key auto_increment,
 nome varchar(40) not null,
 dataNascimento date,
 Profissao varchar(35) not null,
-idioma varchar(20)
+idioma1 varchar(20),
+idioma2 varchar(20),
+idioma3 varchar(20)
 );
 
 create table Gasto(
@@ -18,3 +20,11 @@ descricao varchar(45),
 fkPessoa int,
 foreign key (fkpessoa) references Pessoa (idPessoa)
 )auto_increment = 1001;
+
+insert into pessoa (nome, dataNascimento, profissao, idioma1, idioma2, idioma3) values
+			('Ricardo', '1998-08-27','Marceneiro','Portugês',),
+			('Mauricio', '1993-03-03','Taxista','italiano'),
+			('Martha', '1995-12-07','Programador','Portugês','espanhol'),
+			('José', '1999-02-29','Musico','Portugês','ingles'),
+			('Ana', '2000-09-17','Publiicidade','Portugês','ingles','alemao');
+       
